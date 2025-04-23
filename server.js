b73adb1626f680382be33b8b32c7d2dab34a1c0d
Caysen0907 <queso7216@gmail.com>
2025-04-23 13:37:02 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,56 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const rooms = {};
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('joinGame', () => {
+    let room = Object.keys(rooms).find(r => rooms[r].length < 2);
+    if (!room) {
+      room = socket.id;
+      rooms[room] = [];
+    }
+    rooms[room].push(socket.id);
+    socket.join(room);
+    socket.roomId = room;
+
+    const playerNumber = rooms[room].length;
+    socket.emit('playerNumber', playerNumber);
+    console.log(`Player ${playerNumber} joined room ${room}`);
+
+    if (rooms[room].length === 2) {
+      io.to(room).emit('startGame');
+    }
+  });
+
+  socket.on('roll', () => {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    io.to(socket.roomId).emit('rolled', roll, socket.id);
+  });
+
+  socket.on('placeDie', (col, roll) => {
+    socket.to(socket.roomId).emit('opponentMove', col, roll);
+  });
+
+  socket.on('disconnect', () => {
+    const room = socket.roomId;
+    if (room && rooms[room]) {
+      rooms[room] = rooms[room].filter(id => id !== socket.id);
+      if (rooms[room].length === 0) delete rooms[room];
+      socket.to(room).emit('opponentLeft');
+    }
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
